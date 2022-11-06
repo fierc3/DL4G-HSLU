8f2365fa0e30e0e4d324047fe92afbbda9207da8
BreathingFlesh <chatru.work@gmail.com>
2022-11-06 11:45:55 +0100

    Add some rules for playing cards

--- jass-kit-py/examples/arena/arena_play.py
+++ jass-kit-py/examples/arena/arena_play.py
@@ -9,9 +9,33 @@ import numpy as np
 from jass.agents.agent import Agent
 from jass.arena.arena import Arena
 from jass.agents.agent_random_schieber import AgentRandomSchieber
+from jass.agents.agent_threshold import AgentThreshold
 from jass.game.const import color_masks, card_strings
 from jass.game.game_observation import GameObservation
 from jass.game.rule_schieber import RuleSchieber
+from jass.game.const import *
+from jass.game.game_util import *
+
+
+# score if the color is trump
+trump_score = [15, 10, 7, 25, 6, 19, 5, 5, 5]
+# score if the color is not trump
+no_trump_score = [9, 7, 5, 2, 1, 0, 0, 0, 0]
+# score if obenabe is selected (all colors)
+obenabe_score = [14, 10, 8, 7, 5, 0, 5, 0, 0,]
+# score if uneufe is selected (all colors)
+uneufe_score = [0, 2, 1, 1, 5, 5, 7, 9, 11]
+
+def calculate_trump_selection_score(cards, trump: int) -> int:
+    result = 0
+    for card in cards:
+        color = color_of_card[card]
+        offset = offset_of_card[card]
+        if color == trump:
+            result += trump_score[offset]
+        else:
+            result += no_trump_score[offset]
+    return result
 
 
 class MyAgent(Agent):
@@ -52,7 +76,8 @@ def main():
     # setup the arena
     arena = Arena(nr_games_to_play=1000, save_filename='arena_games')
     player = AgentRandomSchieber()
-    my_player = MyAgent()
+    # player = AgentThreshold()
+    my_player = AgentThreshold()
 
     arena.set_players(my_player, player, my_player, player)
     print('Playing {} games'.format(arena.nr_games_to_play))
