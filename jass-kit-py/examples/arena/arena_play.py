a23e03e6a83c65af66ca42b0a460adac8f189486
Mike / iznogoud / fierc3 <mpullen9@gmail.com>
2022-11-08 23:14:52 +0100

    Fix example arena

--- jass-kit-py/examples/arena/arena_play.py
+++ jass-kit-py/examples/arena/arena_play.py
@@ -9,12 +9,12 @@ import numpy as np
 from jass.agents.agent import Agent
 from jass.arena.arena import Arena
 from jass.agents.agent_random_schieber import AgentRandomSchieber
-from jass.agents.agent_threshold import AgentThreshold
 from jass.game.const import color_masks, card_strings
 from jass.game.game_observation import GameObservation
 from jass.game.rule_schieber import RuleSchieber
 from jass.game.const import *
 from jass.game.game_util import *
+from agent_threshold import AgentThreshold
 
 
 # score if the color is trump
@@ -33,40 +33,44 @@ def calculate_trump_selection_score(cards, trump: int) -> int:
         offset = offset_of_card[card]
         if color == trump:
             result += trump_score[offset]
+        elif trump == OBE_ABE:
+            result += obenabe_score[offset]
+        elif trump == UNE_UFE:
+            result += uneufe_score[offset]
         else:
             result += no_trump_score[offset]
     return result
 
 
-class MyAgent(Agent):
-    """
-    Sample implementation of a player to play Jass.
-    """
-    def __init__(self):
-        # log actions
-        self._logger = logging.getLogger(__name__)
-        # Use rule object to determine valid actions
-        self._rule = RuleSchieber()
-        # init random number generator
-        self._rng = np.random.default_rng()
+# class MyAgent(Agent):
+#     """
+#     Sample implementation of a player to play Jass.
+#     """
+#     def __init__(self):
+#         # log actions
+#         self._logger = logging.getLogger(__name__)
+#         # Use rule object to determine valid actions
+#         self._rule = RuleSchieber()
+#         # init random number generator
+#         self._rng = np.random.default_rng()
 
-    def action_trump(self, obs: GameObservation) -> int:
-        trump = 0
-        max_number_in_color = 0
-        for c in range(4):
-            number_in_color = (obs.hand * color_masks[c]).sum()
-            if number_in_color > max_number_in_color:
-                max_number_in_color = number_in_color
-                trump = c
-        return trump
+#     def action_trump(self, obs: GameObservation) -> int:
+#         trump = 0
+#         max_number_in_color = 0
+#         for c in range(4):
+#             number_in_color = (obs.hand * color_masks[c]).sum()
+#             if number_in_color > max_number_in_color:
+#                 max_number_in_color = number_in_color
+#                 trump = c
+#         return trump
 
-    def action_play_card(self, obs: GameObservation) -> int:
-        # cards are one hot encoded
-        valid_cards = self._rule.get_valid_cards_from_obs(obs)
-        # convert to list and draw a value
-        card = self._rng.choice(np.flatnonzero(valid_cards))
-        self._logger.debug('Played card: {}'.format(card_strings[card]))
-        return card
+#     def action_play_card(self, obs: GameObservation) -> int:
+#         # cards are one hot encoded
+#         valid_cards = self._rule.get_valid_cards_from_obs(obs)
+#         # convert to list and draw a value
+#         card = self._rng.choice(np.flatnonzero(valid_cards))
+#         self._logger.debug('Played card: {}'.format(card_strings[card]))
+#         return card
 
 
 def main():
